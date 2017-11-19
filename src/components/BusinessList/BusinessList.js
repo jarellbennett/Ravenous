0a87e8f0d2c6bc0c36b24fb51b2050296f5fbe13
Jarell <jb123506@gmail.com>
2017-11-18 21:20:29 -0500

    Added this.props to BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,15 +4,15 @@ import {Business} from '../Business/Business';
 //lists businesses
 export class BusinessList extends React.Component{
   render(){
-  return (
-    <div className ="BusinessList">
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-      <Business />
-    </div>
+    return (
+      <div className ="BusinessList">
+      {
+       this.props.businesses.map(business =>
+         {
+           return <Business business={business} />;
+         })
+      }
+      </div>
    );
   }
 }
