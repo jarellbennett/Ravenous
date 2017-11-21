85ae10a4d1630e81c6ecf98dc0d3e9fae853551b
Jarell <jb123506@gmail.com>
2017-11-21 15:06:05 -0500

    Added constructor & methods to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,12 +6,43 @@ const sortByOptions = {
   'Highest Rated': 'rating',
   'Most Reviewed': 'review_count'
 }
+
+
+
+
 //keys() changes object keys to array
 export class SearchBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { term: '' , location: ''  , sortBy: 'best_match' };
+    this.handleLocationChange =this.handleLocationChange.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  getSortByClass(sortByOption) {
+    return this.state.sortBy === sortByOption ? 'active': '';
+  }
+
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy:sortByOption});
+  }
+ //term base on user input
+  handleTermChange(event){
+    this.setState({term: event.target.value});
+  }
+ //location based on what user inputs
+  handleLocationChange(event){
+    this.setState({location: event.target.value})
+  }
+
+
   renderSortByOptions(){
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key='{sortByOptionValue}'>{sortByOption}</li>;
+      return (<li className={this.getSortByClass(sortByOptionValue)}
+      onClick={this.handleSortByChange.bind(this,sortByOptionValue)} >
+      {sortByOption}</li>
+      );
     });
   }
 
@@ -24,11 +55,11 @@ export class SearchBar extends React.Component{
     </ul>
   </div>
   <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
+    <input onChange = {this.handleTermChange} placeholder="Search Businesses" />
+    <input onChange={this.handleLocationChange} placeholder="Where?" />
   </div>
   <div className="SearchBar-submit">
-    <a>Let''s Go</a>
+    <a>Let's Go</a>
   </div>
 </div>
     )
